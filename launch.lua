857107485d8473ff35a2f7a03f282832346a2a10
WriteCoin <sovtsardom@gmail.com>
2021-11-17 18:22:39 +0300

    написан триггер хот-релоада на F7

---
+++ launch.lua
@@ -0,0 +1,19 @@
+HOT_RELOAD_POSTFIX = compiletime(function()
+    HOT_RELOAD_POSTFIX = '_hotreload.txt'
+    return HOT_RELOAD_POSTFIX
+end)
+HOT_RELOAD_START_MODULE = compiletime(function()
+    HOT_RELOAD_START_MODULE = 'src.index'
+    return HOT_RELOAD_START_MODULE
+end)
+
+HOT_RELOAD_TRIGGER = CreateTrigger()
+BlzTriggerRegisterPlayerKeyEvent(HOT_RELOAD_TRIGGER, Player(0), OSKEY_F7, 0, false)
+TriggerAddAction(HOT_RELOAD_TRIGGER, function()
+    local filename = MAP_NAME .. HOT_RELOAD_POSTFIX
+    Preloader(filename)
+    local text = BlzGetAbilityTooltip(FourCC('Agyv'), 0)
+    load(text, '', 't')()
+    require(HOT_RELOAD_START_MODULE)
+    ceres.modules[HOT_RELOAD_START_MODULE].initialized = false
+end)
